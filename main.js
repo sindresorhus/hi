e92e22a7d4495ebc0781e4d8a7cd7927e7f49e24
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-03-06 17:55:55 +0100

    Improve performance of jQuery selector

--- main.js
+++ main.js
@@ -7,8 +7,8 @@
 	};
 
 	// smooth scroll
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 			if (target.length) {
